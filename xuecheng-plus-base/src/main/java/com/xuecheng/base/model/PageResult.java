35b8227cf0393675cb1d66c28a8b349b66ff8b78
July <july_tang77@163.com>
2023-02-17 14:44:18 +0800

    提交父工程、基础工程的基础代码(pom.xml)

---
+++ xuecheng-plus-base/src/main/java/com/xuecheng/base/model/PageResult.java
@@ -0,0 +1,42 @@
+package com.xuecheng.base.model;
+
+import lombok.Data;
+import lombok.ToString;
+
+import java.util.List;
+
+/**
+ * @description 分页查询结果模型类
+ * @author July
+ */
+ @Data
+ @ToString
+public class PageResult<T> {
+    /**
+     * 数据列表
+     */
+    private List<T> items;
+
+    /**
+     * 总记录数
+     */
+    private long counts;
+
+    /**
+     * 当前页码
+     */
+    private long page;
+
+    /**
+     * 每页记录数
+     */
+    private long pageSize;
+
+    public PageResult(List<T> items, long counts, long page, long pageSize) {
+        this.items = items;
+        this.counts = counts;
+        this.page = page;
+        this.pageSize = pageSize;
+    }
+
+}
